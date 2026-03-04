27578380591ee60850955fbb8049cd8095e94161
monihari31 <monihari31@gmail.com>
2026-03-04 16:57:25 -0500

    CORS issue fix

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="AI Career Copilot API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://ai-resume-analyzer-92v2.vercel.app/", "http://localhost:5173",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
